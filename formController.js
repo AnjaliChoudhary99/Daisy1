9b06b396b3ce1fee7a90fc9c4a18b83a117ace8b
Anjali Choudhary <ANJALICHOUDHARY2610@GMAIL.COM>
2024-04-12 01:27:51 +0530

    implemented form controller

--- formController.js
+++ formController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const formController = express.Router();
+const fs = require('fs');
 
 // Define your routes
 formController.get('/', (req, res) => {
@@ -11,5 +12,59 @@ formController.post('/', (req, res) => {
     // Handle POST request
     res.send('POST request received!');
 });
+
+// GET route for form creation
+formController.get('/create/form', (req,res) => {
+    console.log("GET: /create/form");
+
+    //read from file
+    fs.readFile('sampleFormJson.txt', 'utf8', (err, fileContent) => {
+        if (err) {
+          console.log("ERROR: reading from file");
+          console.error(err);
+        } else {
+          console.log("fileContent=" + fileContent);
+          var message = "It is a sample form json, modify it and send a post request:\n\n\n" + fileContent;
+          res.send(message);
+        }
+      });
+
+})
+
+function validationPassed(){
+    return true;
+}
+
+function saveInDb(){
+    return 1;
+}
+
+
+
+formController.post('/create/form', (req,res) => {
+    console.log("POST: /create/form");
+    // body-parser middleware returns 400 bad request if invalid json is there
+
+    // validate the individual data types... LATER
+    if( !validationPassed(req.body) ){
+      console.log("ERROR: validation failed for body = " + JSON.stringify(req.body));
+      res.send("invalid data types");
+    }
+    
+    // save to DB
+    var generatedFormId = saveInDb();
+
+    // trigger the attached use cases
+    
+    // TODO implement this in loop
+    if(req.body.useCasesAttached.smsNotification == "yes"){
+        console.log("sms wanted: yes");
+    }
+    if(req.body.useCasesAttached.gSheetSync == "yes"){
+        console.log("gsheet entry wanted: yes");
+    }
+
+    res.send("request successful & form id = " + generatedFormId);
+})
 
 module.exports = formController;
--- index.js
+++ index.js
@@ -9,7 +9,20 @@ app.set("view engine", "ejs");
 
 
 const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+// Error handling middleware for parsing errors
+app.use((error, req, res, next) => {
+    if (error instanceof SyntaxError && error.status === 400 && 'body' in error) {
+        // Handle parsing error
+        console.error('Error parsing JSON:', error);
+        res.status(400).send('Invalid JSON, error=' + error);
+    } else {
+        // Pass the error to the next middleware
+        next(error);
+    }
+});
 
 // require formcoltroller
 const fc = require('./formController');
@@ -20,5 +33,5 @@ app.use('/', fc);
 
 
 server.listen(port, function(){
-    console.log("Server listening on port ", port);
-  });
+  console.log("Server listening on port ", port);
+});
